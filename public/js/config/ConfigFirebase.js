3f727ce5977ffd598b6075254ffd700cb8f8d58a
DESKTOP-OQUUBQL\jose_ <jose_cg_xd@hotmail.com>
2024-10-19 14:47:24 -0500

    cambios

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBt5o6UoxCG98pvRCvNDAhznpIIDrBTlvY",
+    authDomain: "proyecto1-7af94.firebaseapp.com",
+    projectId: "proyecto1-7af94",
+    storageBucket: "proyecto1-7af94.appspot.com",
+    messagingSenderId: "337984417849",
+    appId: "1:337984417849:web:87ff64f58d62f43abddde8",
+    measurementId: "G-X37YJ6H1EQ"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+  const storage = firebase.storage();
